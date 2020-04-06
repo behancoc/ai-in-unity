65f22103899f2a4a3025492d50f6c1c551ad1ba5
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-04-05 20:56:17 -0700

    Vectors & Moving in a Straight Line

---
+++ moving/moving-project/Assets/Scripts/Move.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+using System.Threading;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public Vector3 goal = new Vector3(5, 0, 4);
+    public float speed = 0.1f;
+
+    void Start() 
+    {
+        
+    }
+
+    private void LateUpdate() 
+    {
+        this.transform.Translate(goal.normalized * speed * Time.deltaTime);
+
+    }
+}
